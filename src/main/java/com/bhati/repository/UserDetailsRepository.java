c52e48ee4dd6b20d120f8e85c0152c2751c61e16
Aman Chugh <aman.chugh@snapdeal.com>
2015-11-25 00:19:24 +0530

    changes for dao

--- src/main/java/com/bhati/repository/UserDetailsRepository.java
+++ src/main/java/com/bhati/repository/UserDetailsRepository.java
@@ -1,14 +1,16 @@
-package com.bhati.dao;
+package com.bhati.repository;
 
 import com.bhati.entity.UserDetails;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by aman on 23/11/15.
  */
-@Repository
 public interface UserDetailsRepository extends CrudRepository<UserDetails,Long> {
 
+    UserDetails findByMobile(Long mobile);
+    List<UserDetails> findByName(Long name);
 }
