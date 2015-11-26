007f282d32b6d07b5d6678635348f9d6838207a6
sumiranchugh <sumiranchugh@gmail.com>
2015-11-26 12:52:40 +0530

    index

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT -Dspring.profiles.active=heroku -jar  target/geoLocation-0.0.1-SNAPSHOT.jar
+web: java -Dserver.port=$PORT -Dspring.profiles.active=heroku -jar  target/GeoLocation-0.0.1-SNAPSHOT.jar
---
+++ src/main/webapp/js/marker.js
@@ -0,0 +1,45 @@
+/**
+ * Created by schug2 on 11/26/2015.
+ */
+$( document ).ready(function() {
+    console.log("ready!");
+
+
+    $("#reload").click(function () {
+
+        location.reload();
+    });
+
+
+});
+
+function initMap() {
+
+    $.get("getGeoAll", function (data) {
+        var response = data;
+        var myLatLng1 = {lat: 28.4469743, lng: 76.9864378};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: myLatLng1,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+
+        for (i = 0; i < response.length; i++) {
+            var myLatLng = {'lat': response[i].latitude, 'lng': response[i].longitude};
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: response[i].id.toString()
+            });
+        }
+
+
+    });
+
+
+    //var myLatLng  = response[i].latitude
+
+}
